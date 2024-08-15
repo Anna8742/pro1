7d0dd92c59a1eaf10192ebf045587b6c8d5efa19
Anna <anna.papka014@gmail.com>
2024-08-15 09:31:59 +0100

    Initializing database when the app starts

--- restaurant_booking/restaurant_booking/reservations/apps.py
+++ restaurant_booking/restaurant_booking/reservations/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from .models import init_db
 
 class ReservationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reservations'
+
+    def ready(self):
+        init_db()
