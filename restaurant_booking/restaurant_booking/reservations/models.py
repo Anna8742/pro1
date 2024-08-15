bcee5caa7eaf4ecf694f75a3f01517473b185c0f
Anna <anna.papka014@gmail.com>
2024-08-15 09:28:45 +0100

    configured SQLAlchemy in models

--- restaurant_booking/restaurant_booking/reservations/models.py
+++ restaurant_booking/restaurant_booking/reservations/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Create your models here.
+DATABASE_URL = "sqlite:///db.sqlite3"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Reservation(Base):
+    __tablename__ = 'reservations'
+
+    id = Column(Integer, primary_key=True, index=True)
+    guest_name = Column(String, nullable=False)
+    guest_count = Column(Integer, nullable=False)
+    reservation_date = Column(DateTime, nullable=False)
+    reservation_time = Column(String, nullable=False)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
