bed12d2132ebcc65797ead26bf06eb654020bed8
Anna <anna.papka014@gmail.com>
2024-08-15 09:34:24 +0100

    Creating reservation form submission

--- restaurant_booking/restaurant_booking/reservations/views.py
+++ restaurant_booking/restaurant_booking/reservations/views.py
@@ -1,3 +1,28 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import SessionLocal, Reservation
+from datetime import datetime
 
-# Create your views here.
+def reservation_form(request):
+    return render(request, 'reservations/form.html')
+
+def create_reservation(request):
+    if request.method == "POST":
+        guest_name = request.POST.get('name')
+        guest_count = request.POST.get('guest_count')
+        reservation_date = request.POST.get('date')
+        reservation_time = request.POST.get('time')
+
+        db = SessionLocal()
+        reservation = Reservation(
+            guest_name=guest_name,
+            guest_count=int(guest_count),
+            reservation_date=datetime.strptime(reservation_date, '%Y-%m-%d'),
+            reservation_time=reservation_time
+        )
+        db.add(reservation)
+        db.commit()
+        db.close()
+
+        return JsonResponse({'message': 'Reservation successful'})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
